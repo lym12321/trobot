35ba6ea27f171060eefaa36238b7f211bebff982
fish <i@coraa.cn>
2025-11-20 21:48:12 +0800

    feat: 为 `bsp/uart` 的异步发送增加 `RingQueue`

---
+++ bsp/src/ds.c
@@ -0,0 +1,43 @@
+/**
+ *  @brief Some Useful DataStruct
+ *  @author tiny_fish
+ *  @date 2025-11-20
+ */
+#include "bsp/ds.h"
+
+#include <string.h>
+
+void ds_rq_init(ds_rq_t *q, uint8_t *buf, const size_t sz) {
+    q->buf = buf;
+    q->sz = sz, q->l = 0, q->r = 0;
+    memset(buf, 0, sz * sizeof(uint8_t));
+}
+
+int ds_rq_size(ds_rq_t *q) {
+    return (q->r - q->l + q->sz) % q->sz;
+}
+
+int ds_rq_avail(ds_rq_t *q) {
+    return q->sz - 1 - ds_rq_size(q);
+}
+
+uint8_t ds_rq_push(ds_rq_t *q, const uint8_t *data, const size_t len) {
+    size_t fs = ds_rq_avail(q);
+    if (len > fs) return 0;
+    for (size_t i = 0; i < len; i++) {
+        q->buf[q->r] = data[i];
+        q->r = (q->r + 1) % q->sz;
+    }
+    return 1;
+}
+
+uint8_t ds_rq_pop(ds_rq_t *q, uint8_t *res, const size_t len) {
+    size_t cs = ds_rq_size(q);
+    if (len > cs) return 0;
+    for (size_t i = 0; i < len; i++) {
+        res[i] = q->buf[q->l];
+        q->buf[q->l] = '_';
+        q->l = (q->l + 1) % q->sz;
+    }
+    return 1;
+}
--- bsp/src/uart.c
+++ bsp/src/uart.c
@@ -3,6 +3,7 @@
 //
 
 #include "bsp/uart.h"
+#include "bsp/ds.h"
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -19,7 +20,11 @@ static bsp_uart_handle_t *handle[BSP_UART_DEVICE_COUNT] = {
     [E_UART_10] = &huart10
 };
 
-static uint8_t lock[BSP_UART_DEVICE_COUNT];
+// Ring Queue for async transmission
+static ds_rq_t rq[BSP_UART_DEVICE_COUNT];
+static volatile uint8_t busy[BSP_UART_DEVICE_COUNT];
+static uint8_t rq_buffer[BSP_UART_DEVICE_COUNT][BSP_UART_BUFFER_SIZE];
+
 static bsp_uart_callback_t callback[BSP_UART_DEVICE_COUNT];
 _ram_d1 static uint8_t tx_buffer[BSP_UART_DEVICE_COUNT][BSP_UART_BUFFER_SIZE];
 _ram_d1 static uint8_t rx_buffer[BSP_UART_DEVICE_COUNT][BSP_UART_BUFFER_SIZE];
@@ -32,26 +37,37 @@ void bsp_uart_send(bsp_uart_e device, const uint8_t *data, const uint32_t len) {
 void bsp_uart_send_async(bsp_uart_e device, const uint8_t *data, const uint32_t len) {
     BSP_ASSERT(0 <= device && device < BSP_UART_DEVICE_COUNT && data != NULL && len > 0);
 
-    // while (lock[device]) __NOP();            // 等待空闲，不会丢包，但一定程度上阻塞
-    if (lock[device]) return;                   // 直接丢弃，会丢包，但不阻塞任务
-    lock[device] = 1;
+    // Init Ring Queue
+    if (rq[device].buf != rq_buffer[device])
+        ds_rq_init(&rq[device], rq_buffer[device], BSP_UART_BUFFER_SIZE);
 
-    if (data != tx_buffer[device]) {
-        memcpy(tx_buffer[device], data, len);
-        data = tx_buffer[device];
-    }
-    if (handle[device]->hdmatx) {
-        HAL_UART_Transmit_DMA(handle[device], data, len);
+    // Full
+    if (ds_rq_avail(&rq[device]) < sizeof(len) + len) return;
+
+    if (!busy[device]) {
+        busy[device] = 1;
+        if (data != tx_buffer[device]) {
+            memcpy(tx_buffer[device], data, len);
+            data = tx_buffer[device];
+        }
+        if (handle[device]->hdmatx) {
+            HAL_UART_Transmit_DMA(handle[device], data, len);
+        } else {
+            HAL_UART_Transmit_IT(handle[device], data, len);
+        }
     } else {
-        HAL_UART_Transmit_IT(handle[device], data, len);
+        ds_rq_push(&rq[device], (const uint8_t *) &len, sizeof(len));
+        ds_rq_push(&rq[device], data, len);
     }
 }
+
+static uint8_t tmp_buffer[BSP_UART_BUFFER_SIZE] = {0};
 
 void bsp_uart_printf(bsp_uart_e device, const char *fmt, ...) {
     BSP_ASSERT(0 <= device && device < BSP_UART_DEVICE_COUNT);
     va_list ap;
     va_start(ap, fmt);
-    uint8_t *buf = tx_buffer[device];
+    uint8_t *buf = tmp_buffer;
     const int len = vsnprintf((char *) buf, BSP_UART_BUFFER_SIZE, fmt, ap);
     va_end(ap);
     BSP_ASSERT(0 < len && len <= BSP_UART_BUFFER_SIZE);
@@ -60,10 +76,9 @@ void bsp_uart_printf(bsp_uart_e device, const char *fmt, ...) {
 
 void bsp_uart_printf_async(bsp_uart_e device, const char *fmt, ...) {
     BSP_ASSERT(0 <= device && device < BSP_UART_DEVICE_COUNT);
-    if (lock[device]) return;
     va_list ap;
     va_start(ap, fmt);
-    uint8_t *buf = tx_buffer[device];
+    uint8_t *buf = busy[device] ? tmp_buffer : tx_buffer[device];
     const int len = vsnprintf((char *) buf, BSP_UART_BUFFER_SIZE, fmt, ap);
     va_end(ap);
     BSP_ASSERT(0 < len && len <= BSP_UART_BUFFER_SIZE);
@@ -95,8 +110,19 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *h, uint16_t len) {
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *h) {
     for (int i = 0; i < BSP_UART_DEVICE_COUNT; i++) {
-        if (handle[i] == h) {
-            lock[i] = 0;
+        if (handle[i] == h && busy[i]) {
+            if (ds_rq_size(&rq[i]) == 0) {
+                busy[i] = 0;
+            } else {
+                uint32_t len = 0;
+                ds_rq_pop(&rq[i], (uint8_t *) &len, sizeof(len));
+                ds_rq_pop(&rq[i], tx_buffer[i], len);
+                if (handle[i]->hdmatx) {
+                    HAL_UART_Transmit_DMA(handle[i], tx_buffer[i], len);
+                } else {
+                    HAL_UART_Transmit_IT(handle[i], tx_buffer[i], len);
+                }
+            }
             break;
         }
     }
