612e4cab200b6ac4116f50f3865f5f00f96d1501
fish <i@coraa.cn>
2026-01-24 15:43:09 +0800

    feat: 增加控制器组件

--- app/main/main.cc
+++ app/main/main.cc
@@ -2,7 +2,6 @@
 // Created by fish on 2025/9/13.
 //
 
-#include <rc/dr16.h>
 
 #include "bsp/bsp.h"
 #include "bsp/led.h"
@@ -10,35 +9,8 @@
 
 #include "bsp/buzzer.h"
 #include "ins/ins.h"
-#include "utils/logger.h"
-#include "utils/vofa.h"
-#include "utils/terminal.h"
+#include "rc/dr16.h"
 #include "utils/os.h"
-
-#include "motor/dm.h"
-
-motor::DM test_dm_motor("test_dm_motor", {
-    .slave_id = 0x01,
-    .master_id = 0x02,
-    .port = E_CAN_1,
-    .mode = motor::DM::MIT,
-    .p_max = 120.f,
-    .v_max = 10.f,
-    .t_max = 10.f
-});
-
-void test(void *args) {
-    test_dm_motor.init();
-    // const auto ins = ins::data();
-    const auto rc = rc::dr16::data();
-    for (;;) {
-        // vofa::send(E_UART_1, ins->roll, ins->pitch, ins->yaw, ins->raw.accel[0], ins->raw.accel[1], ins->raw.accel[2]);
-        // vofa::send(E_UART_1, ins->roll, ins->pitch, ins->yaw);
-        vofa::send(E_UART_1, rc->s_l, rc->s_r, rc->timestamp);
-        bsp_time_delay(1);
-    }
-}
-
 
 extern "C" [[noreturn]] void app_entrance(void *args) {
     bsp_hw_init();
@@ -60,12 +32,9 @@ extern "C" [[noreturn]] void app_entrance(void *args) {
     bsp_time_delay(50);
     bsp_buzzer_flash(4500, 0.2f, 75);
 
-    os::task test_task;
-    test_task.create(test, nullptr, "test", 1024, os::task::Priority::MEDIUM);
-
     for (;;) {
         bsp_led_set_hsv(static_cast<float>(bsp_time_get_ms() % 3000) / 3000.0f, 1.0f, 0.3f);
         bsp_iwdg_refresh();
-        bsp_time_delay(5);
+        os::task::sleep(5);
     }
 }
--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@
 ```shell
 git clone https://github.com/lym12321/trobot.git --recursive
 ```
+
+> [!CAUTION]
+> - 该项目仍在开发中，尚未完成，部分功能可能无法使用。
+> - 此版本相对于前期项目有较大改动，请确保在完全理解项目代码逻辑后使用，**不要**直接复制旧代码的参数和逻辑。
